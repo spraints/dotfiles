b4e273140de023ad1dd759e332b5745db106610b
Matt Burke <spraints@gmail.com>
2021-07-17 15:57:06 -0400

    bark bark

--- dotfiles/shell-functions/devvpn.sh
+++ dotfiles/shell-functions/devvpn.sh
@@ -1,6 +1,7 @@
 devvpn() {
   case "${1:-}" in
     connect)
+      open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-devvpn
       osascript -e 'tell application "Viscosity" to connect "github-iad-devvpn"' ;;
     disconnect)
       osascript -e 'tell application "Viscosity" to disconnect "github-iad-devvpn"' ;;
