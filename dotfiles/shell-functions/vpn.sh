538ff548036bf9b5a066b8b70a7d070102959dcc
Matt Burke <spraints@gmail.com>
2022-03-17 07:53:04 -0400

    add my favorite way to connect to the vpn!

---
+++ dotfiles/shell-functions/vpn.sh
@@ -0,0 +1,12 @@
+vpn() {
+  case "${1:-}" in
+    dev*)
+      open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-devvpn
+      osascript -e 'tell application "Viscosity" to connect "github-iad-devvpn"' ;;
+    iad|prod)
+      open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-prod
+      osascript -e 'tell application "Viscosity" to connect "github-iad-prod"' ;;
+    *)
+      echo 'Usage: vpn iad|dev' ;;
+  esac
+}
