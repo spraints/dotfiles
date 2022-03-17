1b8e8b8f2b5fc1512dbd3141c0bf70ce41f73d73
Matt Burke <spraints@gmail.com>
2022-03-17 11:19:57 -0400

    Better error handling in 'get_data'

--- dotfiles/bin/github-api.rb
+++ dotfiles/bin/github-api.rb
@@ -11,9 +11,17 @@ def get_data(token:, query:, variables: {}, out: nil)
 
   Timeout.timeout(30.0) do
     Net::HTTP.start("api.github.com", 443, use_ssl: true) do |http|
-      body = http.request(req).body
+      resp = http.request(req)
+      body = resp.body
+      if resp.code != "200"
+        raise "HTTP #{resp.code}: body: #{body}"
+      end
       File.write(out, body) if out
-      JSON.parse(body)
+      begin
+        JSON.parse(body)
+      rescue JSON::ParserError => e
+        raise "error parsing response body: #{body.inspect}"
+      end
     end
   end
 end
