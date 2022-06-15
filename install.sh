89fd65f4bf873a114516243a0b7aabbcbd883edd
Matt Burke <spraints@gmail.com>
2022-06-15 19:05:27 +0000

    auto-install on bpdev

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 set -e
 
 if [ "${CODESPACES}" = "true" ]; then
-  script/install-codespaces
-else
-  echo This only does anything in Codespaces. Try running one of the install scripts.
-  ls script/install-*
+  exec script/install-codespaces
 fi
+
+case "$(hostname)" in
+  *bpdev*github.net)
+    exec script/install-bpdev
+    ;;
+esac
+
+echo This only does anything in Codespaces. Try running one of the install scripts.
+ls script/install-*
