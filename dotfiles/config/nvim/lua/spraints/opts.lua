c6678ce3f1275fd0863d80900bcc0b7910376bb9
Matt Burke <spraints@gmail.com>
2022-12-16 16:27:27 +0000

    Disable the mouse!!!!!!!!!!!!!!!!!

--- dotfiles/config/nvim/lua/spraints/opts.lua
+++ dotfiles/config/nvim/lua/spraints/opts.lua
@@ -3,6 +3,7 @@ vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.modeline = true
 vim.opt.modelines = 2
+vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.shiftwidth = 2
 vim.opt.wrap = false
