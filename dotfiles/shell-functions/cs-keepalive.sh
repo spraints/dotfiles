c654941cbd2f3ab82fc09e7bf10fedaf6f21b238
Matt Burke <spraints@gmail.com>
2023-08-09 09:18:15 -0400

    codespaces doesn't stay alive as long as my ssh connection is running

---
+++ dotfiles/shell-functions/cs-keepalive.sh
@@ -0,0 +1,11 @@
+cs-keepalive() {
+  if [ $# -ne 1 ]; then
+    echo Usage: cs-keepalive CODESPACE_ID
+    gh cs list
+    return
+  fi
+  while gh cs ssh -c "$1" date; do
+    sleep 120
+    echo pinging codespace "$1" ...
+  done
+}
