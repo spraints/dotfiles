11d0a3da53bc453a92d32544819cc526e86b9fa8
Matt Burke <spraints@gmail.com>
2023-08-09 09:18:00 -0400

    more vpn!

--- dotfiles/shell-functions/vpn.sh
+++ dotfiles/shell-functions/vpn.sh
@@ -3,6 +3,9 @@ vpn() {
     dev*)
       open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-devvpn
       osascript -e 'tell application "Viscosity" to connect "github-iad-devvpn"' ;;
+    mgmt)
+      open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-mgmt
+      osascript -e 'tell application "Viscosity" to connect "github-iad-mgmt"' ;;
     iad|prod)
       open -a Google\ Chrome https://fido-challenger.githubapp.com/auth/vpn-prod
       osascript -e 'tell application "Viscosity" to connect "github-iad-prod"' ;;
