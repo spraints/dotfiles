a8209a2f7fc5cbe4b1ac3a88474dc74a6a22f3e0
Matt Burke <spraints@gmail.com>
2024-02-23 11:01:51 -0500

    Actually, there's no carbonblack anymore, so i probably don't need this guard

--- config/xbar/plugins/status.1m.bash
+++ config/xbar/plugins/status.1m.bash
@@ -10,18 +10,6 @@ if [ ! -d "$PLUGIN_DIR" ]; then
   exit 0
 fi
 
-# Only run this plugin at home so that I don't make carbonblack go crazy in
-# case I'm tethering.
-case "$(ipconfig getifaddr en0)" in
-  # too many secrets | too many satellites
-  192.168.164.128|192.168.1.27)
-    ;;
-  *)
-    printf '💼\n---\n(github status not available away from home)\n'
-    exit 0
-    ;;
-esac
-
 export PATH='/usr/local/bin:$PATH'
 
 cd "$PLUGIN_DIR"
