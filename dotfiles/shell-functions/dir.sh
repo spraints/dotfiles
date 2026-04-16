a204bd9eda9250588c013b6867972800d804be0b
Matt Burke <spraints@planetscale.com>
2026-04-16 12:14:30 -0400

    better ranking of choices in 'dir'

--- dotfiles/shell-functions/dir.sh
+++ dotfiles/shell-functions/dir.sh
@@ -3,11 +3,7 @@ __dir_usage() {
 Usage: dir DIR
 where DIR is one of:
   - a directory in the current directory
-  - a directory in HOME
-  - a directory in src/
-  - a directory in src/github.com/
-  - a directory in src/github.com/github/
-  - a directory in src/github.com/spraints/
+  - a subdirectory somewhere in src/
 USAGE
 }
 
@@ -28,16 +24,47 @@ _choose_dir_dest() {
     return 1
   fi
 
+  if [ -d "$1" ]; then
+    echo "$1"
+    return 0
+  fi
+
   case "$1" in
     .dotfiles)
+      # special case
       echo ${HOME}/.dotfiles ;;
     .|..)
       return 1 ;;
     */*)
       echo "$1" ;;
     *)
-      fd --type dir --max-depth 3 "$1" ${HOME}/src | sort -r | head -n 1 ;;
+      fd --type dir --max-depth 3 "$1" "${HOME}/src" | __dir_score "$1" | sort -rn | head -n 1 | awk '{print $2}' ;;
   esac
+}
+
+__dir_score() {
+  local script="$(cat <<SCRIPT
+req = ENV["INPUT"]
+guess = \$_.strip.chomp("/").split("/").reverse
+score =
+  case
+  when guess[0] == req
+    100
+  when guess[2] == "github.com" || guess[1] == "experiments"
+    50
+  when guess[1] == "src"
+    25
+  else
+    0
+  end
+puts "#{score} #\$_"
+SCRIPT
+)"
+  if [ -z "$__DEBUG_DIR" ]; then
+    INPUT="$1" ruby -ne "$script" | tee /dev/stderr
+  else
+    INPUT="$1" ruby -ne "$script" | tee /dev/stderr
+  fi
 }
 
 # ok: zsh
