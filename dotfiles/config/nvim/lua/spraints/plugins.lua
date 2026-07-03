6ffec4d0a0b028a98d88bf793bb1f56b6f7ddc95
Matt Burke <spraints@planetscale.com>
2026-07-03 15:18:02 -0400

    Migrate neovim plugin management from packer to vim.pack
    Neovim 0.12 ships a built-in plugin manager (vim.pack), making packer.nvim
    unnecessary. LSP setup already used the modern vim.lsp.enable() API, so
    that's unchanged.

--- dotfiles/config/nvim/lua/spraints/plugins.lua
+++ dotfiles/config/nvim/lua/spraints/plugins.lua
@@ -1,50 +1,37 @@
--- After changing anything in this file, you probably need to run
--- :PackerSync and choose N when asked about deleteing packer.nvim.
 local M = {}
 
 function M.setup()
-  local function plugins()
-    use {
-      "junegunn/fzf",
-      run = "./install --all",
-      event = "VimEnter",
-    }
+  vim.pack.add({
+    "https://github.com/junegunn/fzf",
+    "https://github.com/junegunn/fzf.vim",
+    "https://github.com/mason-org/mason.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+  })
 
-    use {
-      "junegunn/fzf.vim",
-      event = "BufEnter",
-    }
+  -- fzf's own install script builds the fzf binary; vim.pack has no
+  -- build-hook field, so run it via the PackChanged event instead.
+  vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+      local data = ev.data
+      if data.spec.name == "fzf" and (data.kind == "install" or data.kind == "update") then
+        vim.system({ "./install", "--all" }, { cwd = data.path }):wait()
+      end
+    end,
+  })
 
-    use {
-      "mason-org/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end,
-    }
+  require("mason").setup()
 
-    use {
-      "neovim/nvim-lspconfig",
-      requires = {
-        "mason-org/mason.nvim",
-      },
-      config = function()
-        -- See https://vonheikemen.github.io/learn-nvim/feature/lsp-setup.html#lsp-defaults
-        -- for keymaps.
-        --
-        -- See
-        -- https://github.com/mjlbach/starter.nvim/blob/6a8329eb0874014bcde92e7f78fe3176595e5a45/init.lua#L225
-        -- for more keymap ideas.
-        vim.lsp.enable("gopls")
-        vim.lsp.enable("protols")
-        vim.lsp.enable("rust_analyzer")
-        -- vim.lsp.enable("ts_ls")
-        vim.lsp.enable("zls")
-      end,
-    }
-  end
-
-  local packer = require("packer")
-  packer.startup(plugins)
+  -- See https://vonheikemen.github.io/learn-nvim/feature/lsp-setup.html#lsp-defaults
+  -- for keymaps.
+  --
+  -- See
+  -- https://github.com/mjlbach/starter.nvim/blob/6a8329eb0874014bcde92e7f78fe3176595e5a45/init.lua#L225
+  -- for more keymap ideas.
+  vim.lsp.enable("gopls")
+  vim.lsp.enable("protols")
+  vim.lsp.enable("rust_analyzer")
+  -- vim.lsp.enable("ts_ls")
+  vim.lsp.enable("zls")
 end
 
 return M
